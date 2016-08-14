e37d43c12f5f3e010c593f83f7afdcdb1a616c11
RoyLzr <liuzhaorui1@163.com>
2016-08-14 20:17:26 +0800

    HandleDJEvent

--- src/kn/main/server/event_type/HandleDJEvents.java
+++ src/kn/main/server/event_type/HandleDJEvents.java
@@ -1,7 +1,42 @@
 package kn.main.server.event_type;
 
+
 /**
  * Created by zhangjie on 8/13/16.
  */
 public class HandleDJEvents {
+    public int Handle(DJEventMsg msg)
+    {
+        int status = 0;
+
+        switch (msg.getEvent()) {
+            case EventType.DJ_SHARE_START_EVT:
+                HandleDjStart(msg);
+                break;
+            case EventType.DJ_SHARE_STOP_EVT:
+                break;
+            default:
+                status = -1;
+        }
+        return status;
+    }
+
+    protected int HandleDjStart(DJEventMsg event)
+    {
+        int status = 0;
+
+        do {
+            synchronized() {
+                if(null == djFollowersMap)
+                if (djFollowersMap.containsKey(clientFd)) {
+                    //System.out.println("fd is Dj, Dj index: " + clientFd);
+                    status = -1;
+                }
+                else{
+
+                }
+            }
+        } whlie(0);
+
+    }
 }
