cbd9efc1043ee787e450ee67c768d0585695e312
evannzhang(张楠) <evannzhang@tencent.com>
2016-08-14 17:44:29 +0800

    数据库池

--- src/kn/main/server/msg_type/InteractEventMsg.java
+++ src/kn/main/server/msg_type/InteractEventMsg.java
@@ -6,5 +6,31 @@ package kn.main.server.msg_type;
  * @see kn.main.common.EventType
  */
 public class InteractEventMsg {
+    int event;
+    int djID;
+    int uID;
 
+    public int getDjID() {
+        return djID;
+    }
+
+    public void setDjID(int djID) {
+        this.djID = djID;
+    }
+
+    public int getEvent() {
+        return event;
+    }
+
+    public void setEvent(int event) {
+        this.event = event;
+    }
+
+    public int getuID() {
+        return uID;
+    }
+
+    public void setuID(int uID) {
+        this.uID = uID;
+    }
 }
